8dcf6eff04fbc8bfb1bc330a9113961963707374
AnLe <thanhan.le21@gmail.com>
2021-08-18 09:40:34 +0700

    SharedPrefs

---
+++ app/src/main/java/com/frankdev/cleanarchitecturekotlin/presentation/App.kt
@@ -0,0 +1,10 @@
+package com.frankdev.cleanarchitecturekotlin.presentation
+
+import android.app.Application
+
+class App : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
